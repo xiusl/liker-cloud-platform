9d4b0151cfd68d2cbc0b3921c746bedab469ae8a
xiusl <xiushilin@hotmail.com>
2022-03-01 11:35:36 +0800

    upgrade spring

---
+++ auth/src/main/java/com/liker/cloud/auth/config/TokenStoreConfig.java
@@ -0,0 +1,26 @@
+package com.liker.cloud.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+
+@Configuration
+public class TokenStoreConfig {
+
+    private static final String SIGN_KEY = "123123";
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey(SIGN_KEY);
+        return jwtAccessTokenConverter;
+    }
+}
